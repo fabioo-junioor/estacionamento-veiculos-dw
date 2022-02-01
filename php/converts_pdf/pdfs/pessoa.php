75ea311baae83b15a5ecaf669346ccb2585e50df
FabioJr <fj_011@hotmail.com>
2022-02-01 01:02:32 -0300

    Alterações descritas no README.md

--- php/converts_pdf/pdfs/pessoa.php
+++ php/converts_pdf/pdfs/pessoa.php
@@ -21,13 +21,21 @@ include('../conexao.php');
     }
 
     tabled td, table th{
-        border: 1px solid #ccc;
+        border: 2px solid #000000;
+        background-color: #a0a0a0;
     }
 
     tabled th{
         font-weight: bold;
         background-color: #eee;
         padding: 10px;
+        
+    }
+    tbody td{
+        border: 1px solid #c3c3c3;
+        padding: 10px;
+        color: #000000;
+        
     }
 
 </style>
